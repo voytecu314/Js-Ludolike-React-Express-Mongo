ceabd8f29fd4ca4d60c210452a1ee840b9fd7d24
WojtekU <voytecu@gmail.com>
2022-06-14 14:37:18 +0200

    local server (port5001) connected to Mongo Atlas, client empty

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express =  require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
+
+dotenv.config();
 const port = process.env.PORT || 5000;
+const DB_URL= process.env.DB_URL;
+
+mongoose.connect(DB_URL).then(()=>console.log('Connected to DB')).catch(err=>console.log('DB -initial connection error: ',err.message));
+
+//To handle errors after initial connection was established
+mongoose.connection.on('disconnected', () => { console. log('DB-> lost connection'); }); 
 
 app.listen(port,()=>{
 
